3ca4bb3dffa87898d89215196d6eb4da73c14339
oleg <58697249+zerex21@users.noreply.github.com>
2024-02-07 13:37:00 +0300

    feat: completed test for 1

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -5,7 +5,7 @@ const testCases = [
     { a: 1, b: 2, action: Action.Add, expected: 3 },
     { a: 2, b: 2, action: Action.Add, expected: 4 },
     { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
+    // continue cases for other actions
 ]; */
 
 describe('simpleCalculator', () => {
